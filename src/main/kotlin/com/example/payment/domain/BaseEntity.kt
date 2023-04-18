bbbe0ad586cada1fd1cd72061d62514fb00622c3
choidongkuen <danaver12@daum.net>
2023-04-19 00:38:12 +0900

    [001-DOMAIN_IMPLEMENT] Feat : 필요한 엔티티 클래스 구현
    BaseEntity,Order,OrderTransaction,PaymentUser 구현

---
+++ src/main/kotlin/com/example/payment/domain/BaseEntity.kt
@@ -0,0 +1,17 @@
+package com.example.payment.domain
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
+    @CreatedDate
+    lateinit var createdAt: LocalDateTime
+
+    @LastModifiedDate
+    lateinit var updatedAt: LocalDateTime
+}
