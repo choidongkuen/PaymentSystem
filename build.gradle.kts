b09281c110816466b777d72bdfac017067d819ad
choidongkuen <danaver12@daum.net>
2023-04-19 00:37:23 +0900

    [001-DOMAIN_IMPLEMENT] Feat : build.gradle.kts 필요한 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     id("org.springframework.boot") version "2.7.10"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "com.example"
@@ -16,28 +15,24 @@ configurations {
         extendsFrom(configurations.annotationProcessor.get())
     }
 }
-extra["springCloudVersion"] = "2021.0.4"
+
 repositories {
     mavenCentral()
 }
 
+extra["springCloudVersion"] = "2021.0.6"
+
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
-    }
 }
 
 dependencyManagement {
@@ -45,6 +40,9 @@ dependencyManagement {
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
     }
 }
+
+
+
 tasks.withType<Test> {
     useJUnitPlatform()
 }
