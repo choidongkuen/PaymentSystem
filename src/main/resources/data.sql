8bde56a7c7a40195fcde9a7fdb9282ab42b6fcbb
choidongkuen <danaver12@daum.net>
2023-04-20 02:45:49 +0900

    [008-API_07] Feat : data.sql 을 통해 예비 데이터 미리 넣기
    application.yml spring.jpa.defer-datasource-initialization : true 필수

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into payment_user
+(id, payment_user_id, account_user_id, account_number, name, created_at, updated_at)
+values (1, 'paymentUserId01', 1, '1000000000', '최동근', CURRENT_TIMESTAMP, current_timestamp);
