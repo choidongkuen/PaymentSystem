013507b2eb23fff8d8391d1724826769278b4a6c
choidongkuen <danaver12@daum.net>
2023-04-19 16:58:30 +0900

    [005-API_04] Feat : PaymentService's pay 함수 계좌 요청시 성공인 경우 코득 구현

--- src/main/kotlin/com/example/payment/repository/PaymentUserRepositories.kt
+++ src/main/kotlin/com/example/payment/repository/PaymentUserRepositories.kt
@@ -3,6 +3,7 @@ package com.example.payment.repository
 import com.example.payment.domain.Order
 import com.example.payment.domain.OrderTransaction
 import com.example.payment.domain.PaymentUser
+import com.example.payment.domain.TransactionType
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface PaymentUserRepository : JpaRepository<PaymentUser, Long> {
@@ -14,5 +15,8 @@ interface OrderRepository : JpaRepository<Order, Long> {
 }
 
 interface OrderTransactionRepository : JpaRepository<OrderTransaction, Long> {
-
+    fun findByOrderAndTransactionType(
+        order: Order,
+        transactionType: TransactionType
+    ): List<OrderTransaction>
 }
